9ff3868699c65eb4f4e0847d07ef2f0d9afd5839
Bruno Agatte <bruno.agatte@backstage.com>
2018-06-01 09:39:19 -0400

    basic project structure implemented

---
+++ Stash/Stash/Features/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Stash
+//
+//  Created by Bruno Agatte on 6/1/18.
+//  Copyright © 2018 Bruno Agatte. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
